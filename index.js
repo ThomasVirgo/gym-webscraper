cb5280fe223b94d5e92de62fabd572229ac4face
Thomas Virgo <tomcvirgo@gmail.com>
2021-04-17 20:36:35 +0100

    added zing chart to track busyness

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import getData from './lib/getData.js';
+import './lib/cron.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const idxPath = __dirname + '/views/index.html';
 
+app.use(express.static('lib'));
+
 app.get('/', (req,res)=>{
     res.sendFile(idxPath);
+});
+
+app.get('/data', (req,res)=>{
+    let data = getData();
+    res.json(data);
 })
 
 app.listen(PORT, ()=>{console.log(`Server is running. Listening on ${PORT}`)});
